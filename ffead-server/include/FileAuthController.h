bd475428c8aa7a54b6d4f5cfbf032c6a7f954323
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2010-11-25 10:49:30 +0000

    New Default File based basic authorization handler

---
+++ ffead-server/include/FileAuthController.h
@@ -0,0 +1,28 @@
+/*
+ * FileAuthController.h
+ *
+ *  Created on: Nov 23, 2010
+ *      Author: sumeet
+ */
+
+#ifndef FILEAUTHCONTROLLER_H_
+#define FILEAUTHCONTROLLER_H_
+#include "AuthController.h"
+#include "fstream"
+
+class FileAuthController :public AuthController
+{
+	string filename;
+	string delimiter;
+public:
+	string treat_password(string);
+	FileAuthController(string,string);
+	virtual ~FileAuthController();
+	bool authenticate(string,string);
+	bool isInitialized();
+	bool authenticate(map<string,string>){return false;};
+	bool handle(HttpRequest* req,HttpResponse* res){return false;};
+	bool getPassword(string username,string &passwd);
+};
+
+#endif /* FILEAUTHCONTROLLER_H_ */
