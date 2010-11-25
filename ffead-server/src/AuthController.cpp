fd76cf4185bee0852b83dc697ebfc96ae9d0b775
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2010-11-25 11:51:44 +0000

    New Authorization Interface Handler

---
+++ ffead-server/src/AuthController.cpp
@@ -0,0 +1,17 @@
+/*
+ * AuthController.cpp
+ *
+ *  Created on: Nov 23, 2010
+ *      Author: sumeet
+ */
+
+#include "AuthController.h"
+
+AuthController::AuthController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AuthController::~AuthController() {
+	// TODO Auto-generated destructor stub
+}
