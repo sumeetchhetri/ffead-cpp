773d5cafc0e9ba62ab80a8063a976a1ada045ee4
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2010-11-25 10:54:20 +0000

    New Authorization Interface Handler

---
+++ ffead-server/include/AuthController.h
@@ -0,0 +1,25 @@
+/*
+ * AuthController.h
+ *
+ *  Created on: Nov 23, 2010
+ *      Author: sumeet
+ */
+
+#ifndef AUTHCONTROLLER_H_
+#define AUTHCONTROLLER_H_
+#include "string"
+#include "map"
+#include "HttpRequest.h"
+#include "HttpResponse.h"
+using namespace std;
+class AuthController {
+public:
+	AuthController();
+	virtual ~AuthController();
+	virtual bool authenticate(string user,string password)=0;
+	virtual bool authenticate(map<string,string>)=0;
+	virtual bool handle(HttpRequest* req,HttpResponse* res)=0;
+	virtual bool isInitialized()=0;
+};
+
+#endif /* AUTHCONTROLLER_H_ */
