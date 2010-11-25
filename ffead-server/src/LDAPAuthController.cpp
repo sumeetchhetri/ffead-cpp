522362973d546de6fff95d0fffe97a18aa1b92ed
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2010-11-25 12:43:38 +0000

    Empty Default LDAP based basic authorization handler

---
+++ ffead-server/src/LDAPAuthController.cpp
@@ -0,0 +1,17 @@
+/*
+ * LDAPAuthController.cpp
+ *
+ *  Created on: Nov 23, 2010
+ *      Author: sumeet
+ */
+
+#include "LDAPAuthController.h"
+
+LDAPAuthController::LDAPAuthController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LDAPAuthController::~LDAPAuthController() {
+	// TODO Auto-generated destructor stub
+}
