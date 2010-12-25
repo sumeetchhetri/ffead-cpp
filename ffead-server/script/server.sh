0aa3f59982e541268e07d07a609b8a7875a5c4ae
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2010-12-25 06:29:29 +0000

    clean the temporrary session data

--- ffead-server/script/server.sh
+++ ffead-server/script/server.sh
@@ -5,6 +5,7 @@ export LD_LIBRARY_PATH=$FEAD_CPP_PATH/lib:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 rm -f $FEAD_CPP_PATH/rtdcf/*.d $FEAD_CPP_PATH/rtdcf/*.o $FEAD_CPP_PATH/lib/libinter.so 
 rm -f $FEAD_CPP_PATH/*.cntrl
+rm -f $FEAD_CPP_PATH/tmp/*.sess
 chmod 700 $FEAD_CPP_PATH/resources/run.sh
 chmod 700 $FEAD_CPP_PATH/resources/rundyn.sh
 #/usr/sbin/setenforce 0
