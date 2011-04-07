6773a6127bbed6e14119ef7164adc8e3d795fb93
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2011-04-07 08:42:51 +0000

    Dont delete the libinter.so file on every restart, check the COMPILE_MODE flag to control thios beahviour, moved the delete code to run.sh

--- ffead-server/script/server.sh
+++ ffead-server/script/server.sh
@@ -3,7 +3,7 @@ export FEAD_CPP_PATH=`echo $(dirname $(readlink -f $0))`
 echo $FEAD_CPP_PATH
 export LD_LIBRARY_PATH=$FEAD_CPP_PATH/lib:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
-rm -f $FEAD_CPP_PATH/rtdcf/*.d $FEAD_CPP_PATH/rtdcf/*.o $FEAD_CPP_PATH/lib/libinter.so 
+rm -f $FEAD_CPP_PATH/rtdcf/*.d $FEAD_CPP_PATH/rtdcf/*.o 
 rm -f $FEAD_CPP_PATH/*.cntrl
 rm -f $FEAD_CPP_PATH/tmp/*.sess
 chmod 700 $FEAD_CPP_PATH/resources/run.sh
