3260b84faa6714d0b6e566e9b406271fadee808b
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-04-21 08:01:07 +0000

    Move the branch winx/cygwin version to trunk and add README and INSTALL files, also remove duplicate subdirl.mk files, comment the no arguments REST method dependency in CHServer.cpp

--- ffead-server-unix/Debug/src/subdirl.mk
+++ ffead-server-unix/Debug/src/subdirl.mk
@@ -260,7 +260,7 @@ CPP_DEPS += \
 %.o: ../../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -l ../src/ -I../include -O0 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -l ../src/ -I../include -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- ffead-server-unix/src/CHServer.cpp
+++ ffead-server-unix/src/CHServer.cpp
@@ -2399,8 +2399,8 @@ strVec temporaray(strVec webdirs,strVec webdirs1,string incpath,string rtdcfpath
 											restfunction.params.push_back(param);
 										}
 									}
-									if(restfunction.params.size()==0)
-										continue;
+									//if(restfunction.params.size()==0)
+									//	continue;
 									if(clas!="")
 									{
 										if(url.find("*")==string::npos)
