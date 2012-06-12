2485744ea50949f9c7d586d20e7b8bdcd91d433a
sumeet.chhetri@gmail.com <sumeet.chhetri@gmail.com@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-06-12 05:48:00 +0000

    added date arithmetic operations...

--- include/Date.h
+++ include/Date.h
@@ -39,6 +39,129 @@ class Date {
 	string ss;
 	string dayw;
 	string getMon(string);
+	long getDays(long y,long m,long d)
+	{
+		y = y -1900;
+		m = (m + 9) % 12;
+		y = y - m/10;
+		return 365*y + y/4 - y/100 + y/400 + (m*306 + 5)/10 + ( d - 1 );
+	}
+
+	long getHours(long y,long m,long d, long hh)
+	{
+		long g = getDays(y,m,d);
+		int hfd = hh/24;
+		hh = hh%24;
+		return (g+hfd)*24 + hh;
+	}
+
+	long getMinutes(long y,long m,long d, long hh, long mi)
+	{
+		long g = getHours(y,m,d,hh);
+		int mfh = mi/60;
+		mi = mi%60;
+		return (g+mfh)*60 + mi;
+	}
+
+	unsigned long long getSeconds(long y,long m,long d, long hh, long mi, long ss)
+	{
+		long g = getMinutes(y,m,d,hh,mi);
+		int sfm = ss/60;
+		ss = ss%60;
+		return (g+sfm)*60 + ss;
+	}
+
+	void getDateFromDays(long days)
+	{
+		long y = (10000*days + 14780)/3652425;
+		long ddd = days - (365*y + y/4 - y/100 + y/400);
+		if (ddd < 0)
+		{
+			y = y - 1;
+			ddd = days - (365*y + y/4 - y/100 + y/400);
+		}
+		long mi = (100*ddd + 52)/3060;
+		long mm = (mi + 2)%12 + 1;
+		y = y + (mi + 2)/12 + 1900;
+		long dd = ddd - (mi*306 + 5)/10 + 1;
+		cout << y << " " << mm << " " << dd;
+	}
+
+	void getDateFromHours(long hours)
+	{
+		long days = hours/24;
+		hours = hours%24;
+		long y = (10000*days + 14780)/3652425;
+		long ddd = days - (365*y + y/4 - y/100 + y/400);
+		if (ddd < 0)
+		{
+			y = y - 1;
+			ddd = days - (365*y + y/4 - y/100 + y/400);
+		}
+		long mi = (100*ddd + 52)/3060;
+		long mm = (mi + 2)%12 + 1;
+		y = y + (mi + 2)/12 + 1900;
+		long dd = ddd - (mi*306 + 5)/10 + 1;
+		cout << y << " " << mm << " " << dd << " " << hours;
+	}
+
+	void getDateFromMinutes(long long minutes)
+	{
+		long hours = minutes/60;
+		minutes = minutes%60;
+		long days = hours/24;
+		hours = hours%24;
+		long y = (10000*days + 14780)/3652425;
+		long ddd = days - (365*y + y/4 - y/100 + y/400);
+		if (ddd < 0)
+		{
+			y = y - 1;
+			ddd = days - (365*y + y/4 - y/100 + y/400);
+		}
+		long mi = (100*ddd + 52)/3060;
+		long mm = (mi + 2)%12 + 1;
+		y = y + (mi + 2)/12 + 1900;
+		long dd = ddd - (mi*306 + 5)/10 + 1;
+		cout << y << " " << mm << " " << dd << " " << hours << ":" << minutes;
+	}
+
+	void getDateFromSeconds(long long seconds)
+	{
+		long long minutes = seconds/60;
+		seconds = seconds%60;
+		long hours = minutes/60;
+		minutes = minutes%60;
+		long days = hours/24;
+		hours = hours%24;
+		long y = (10000*days + 14780)/3652425;
+		long ddd = days - (365*y + y/4 - y/100 + y/400);
+		if (ddd < 0)
+		{
+			y = y - 1;
+			ddd = days - (365*y + y/4 - y/100 + y/400);
+		}
+		long mi = (100*ddd + 52)/3060;
+		long mm = (mi + 2)%12 + 1;
+		y = y + (mi + 2)/12 + 1900;
+		long dd = ddd - (mi*306 + 5)/10 + 1;
+		cout << y << " " << mm << " " << dd << " " << hours << ":" << minutes << ":" << seconds <<  "\n" << endl << flush;
+	}
+
+	int test()
+	{
+		unsigned long long gg = getDays(2008,12,31);
+		getDateFromDays(gg);
+		cout << endl;
+		gg = getHours(2008,12,31,12);
+		getDateFromHours(gg);
+		cout << endl;
+		gg = getMinutes(2008,12,31,12,56);
+		getDateFromMinutes(gg);
+		cout << endl;
+		gg = getSeconds(2008,12,31,12,56,56);
+		getDateFromSeconds(gg);
+		return 0;
+	}
 public:
 	Date();
 	virtual ~Date();
