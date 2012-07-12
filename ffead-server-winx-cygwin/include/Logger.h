e13b1a1230bcd6a81efaa3692d1d00a0637cd8d0
sumeet.chhetri@gmail.com <sumeet.chhetri@gmail.com@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-07-12 20:31:19 +0000

    Updated new logging system

--- ffead-server-winx-cygwin/include/Logger.h
+++ ffead-server-winx-cygwin/include/Logger.h
@@ -1,17 +1,17 @@
 /*
-	Copyright 2010, Sumeet Chhetri 
-  
-    Licensed under the Apache License, Version 2.0 (the "License"); 
-    you may not use this file except in compliance with the License. 
-    You may obtain a copy of the License at 
-  
-        http://www.apache.org/licenses/LICENSE-2.0 
-  
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, 
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-    See the License for the specific language governing permissions and 
-    limitations under the License.  
+	Copyright 2010, Sumeet Chhetri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 /*
  * Logger.h
@@ -24,29 +24,38 @@
 #define LOGGER_H_
 #include "PropFileReader.h"
 #include "DateFormat.h"
+#include "queue.h"
 #include <boost/thread/thread.hpp>
 class Logger {
 public:
-	static Logger* getLogger();
-	static void info(string);
-	static void debug(string);
-	static void error(string);
+	static string LEVEL_ERROR;
+	static string LEVEL_DEBUG;
+	static string LEVEL_INFO;
+	static Logger getLogger(string className);
+	void info(string);
+	void debug(string);
+	void error(string);
 	static void init();
 	static void init(string file);
 	static void init(string level,string mode,string file);
-private:
-	virtual ~Logger();
 	Logger();
+	virtual ~Logger();
+	template <typename T>
+	friend Logger& operator<< (Logger& logger, T msg);
+	friend Logger& operator<< (Logger& logger, ostream& (*pf) (ostream&));
+private:
 	Logger(string);
 	Logger(string,string,string);
-	DateFormat datFormat;
-	string level;
-	string mode;
-	string filepath;
-	ofstream out;
+	string className;
+	static DateFormat* datFormat;
+	static string* level;
+	static string* mode;
+	static string* filepath;
+	static ofstream* out;
 	void write(string,string);
-	boost::mutex p_mutex;
-	static Logger* m_pInstance;
+	template <typename T>
+	void write(T tmsg,string mod);
+	void write(ostream& (*pf) (ostream&), string mod);
+	static boost::mutex* _theLogmutex;
 };
-
 #endif /* LOGGER_H_ */
--- ffead-server-winx-cygwin/src/Logger.cpp
+++ ffead-server-winx-cygwin/src/Logger.cpp
@@ -1,17 +1,17 @@
 /*
-	Copyright 2010, Sumeet Chhetri 
-  
-    Licensed under the Apache License, Version 2.0 (the "License"); 
-    you may not use this file except in compliance with the License. 
-    You may obtain a copy of the License at 
-  
-        http://www.apache.org/licenses/LICENSE-2.0 
-  
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, 
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-    See the License for the specific language governing permissions and 
-    limitations under the License.  
+	Copyright 2010, Sumeet Chhetri
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 */
 /*
  * Logger.cpp
@@ -23,111 +23,181 @@
 #include "Logger.h"
 
 // Global static pointer used to ensure a single instance of the class.
-Logger* Logger::m_pInstance = NULL;
+boost::mutex* Logger::_theLogmutex = NULL;
+string* Logger::level = NULL;
+string* Logger::mode = NULL;
+DateFormat* Logger::datFormat = NULL;
+ofstream* Logger::out = NULL;
 
-Logger* Logger::getLogger()
+string Logger::LEVEL_ERROR = "ERROR";
+string Logger::LEVEL_DEBUG = "DEBUG";
+string Logger::LEVEL_INFO = "INFO";
+
+Logger Logger::getLogger(string className)
 {
-	if(m_pInstance==NULL)
-	{
-		m_pInstance = new Logger();
-	}
-	return m_pInstance;
+	Logger logger(className);
+	return logger;
 }
 
 void Logger::init()
 {
-	if(m_pInstance==NULL)
+	if(_theLogmutex==NULL)
 	{
-		m_pInstance = new Logger();
+		_theLogmutex = new boost::mutex();
+		level = new string(LEVEL_ERROR);
+		mode = new string("CONSOLE");
+		datFormat = new DateFormat("dd/mm/yyyy hh:mi:ss");
 	}
 }
 
 void Logger::init(string file)
 {
-	if(m_pInstance==NULL)
+	if(_theLogmutex==NULL)
 	{
-		m_pInstance = new Logger(file);
+		_theLogmutex = new boost::mutex();
+
+		PropFileReader pf;
+		propMap props = pf.getProperties(file);
+		if(props.size()==0)
+		{
+			level = new string(LEVEL_ERROR);
+			mode = new string("CONSOLE");
+			datFormat = new DateFormat("dd/mm/yyyy hh:mi:ss");
+			return;
+		}
+		level = new string(props["LEVEL"]);
+		mode = new string(props["MODE"]);
+		filepath = new string(props["FILEPATH"]);
+		datFormat = new DateFormat(props["DATEFMT"]);
+		if(*mode=="FILE")
+		{
+			out = new ofstream();
+			out->open(filepath->c_str(),ios::app | ios::binary);
+		}
 	}
 }
 
-void Logger::init(string level,string mode,string file)
+void Logger::init(string llevel,string lmode,string lfilepath)
 {
-	if(m_pInstance==NULL)
+	if(_theLogmutex==NULL)
 	{
-		m_pInstance = new Logger(level,mode,file);
+		_theLogmutex = new boost::mutex();
+
+		level = new string(llevel);
+		mode = new string(lmode);
+		filepath = new string(lfilepath);
+		datFormat = new DateFormat("dd/mm/yyyy hh:mi:ss");
+		if(*mode=="FILE")
+		{
+			out = new ofstream();
+			out->open(filepath->c_str(),ios::app | ios::binary);
+		}
 	}
 }
 
-Logger::Logger()
+Logger::Logger(string className)
 {
-	PropFileReader pf;
-	level = "ERROR";
-	mode = "CONSOLE";
-	datFormat.setFormatspec("dd/mm/yyyy hh:mi:ss");
-}
-
-Logger::Logger(string file)
-{
-	PropFileReader pf;
-	propMap props = pf.getProperties(file);
-	if(props.size()==0)
-	{
-		level = "ERROR";
-		mode = "CONSOLE";
-		datFormat.setFormatspec("dd/mm/yyyy hh:mi:ss");
-		return;
-	}
-	level = props["LEVEL"];
-	mode = props["MODE"];
-	filepath = props["FILEPATH"];
-	if(mode=="FILE")
-		out.open(filepath.c_str(),ios::app | ios::binary);
-	datFormat.setFormatspec(props["DATEFMT"]);
-}
-Logger::Logger(string level,string mode,string filepath)
-{
-	this->level = level;
-	this->mode = mode;
-	this->filepath = filepath;
+	this->className = className;
 }
 
 Logger::~Logger()
 {
-	out.close();
+	if(_theLogmutex!=NULL)
+	{
+		delete _theLogmutex;
+		delete level;
+		delete mode;
+		delete datFormat;
+		if(filepath!=NULL)delete filepath;
+		if(out!=NULL)delete out;
+	}
 }
 
 void Logger::write(string msg,string mod)
 {
 	Date dat;
-	string te = this->datFormat.format(dat);
-	if(mode=="FILE")
+	string te = datFormat->format(dat);
+	msg = "[" + te + "] ("+this->className + ") <"+mod+"> :"+msg+"\n";
+	if(*mode=="FILE")
 	{
-		msg = "[" + te + "] <"+mod+"> :"+msg+"\n";
-		m_pInstance->p_mutex.lock();
-		m_pInstance->out.write(msg.c_str(),msg.length());
-		m_pInstance->out << flush;
-		m_pInstance->p_mutex.unlock();
+		_theLogmutex->lock();
+		out->write(msg.c_str(),msg.length());
+		*out << flush;
+		_theLogmutex->unlock();
 	}
 	else
 	{
-		msg = "[" + te + "] <"+mod+"> :"+msg+"\n";
-		m_pInstance->p_mutex.lock();
+		_theLogmutex->lock();
 		cout << msg << flush;
-		m_pInstance->p_mutex.unlock();
+		_theLogmutex->unlock();
+	}
+}
+
+template <typename T>
+void Logger::write(T tmsg, string mod)
+{
+	Date dat;
+	string te = datFormat->format(dat);
+	string msg = "[" + te + "] ("+this->className + ") <"+mod+"> :";
+	if(*mode=="FILE")
+	{
+		_theLogmutex->lock();
+		*out << msg << tmsg << endl;
+		_theLogmutex->unlock();
+	}
+	else
+	{
+		_theLogmutex->lock();
+		cout << msg << tmsg << endl;
+		_theLogmutex->unlock();
+	}
+}
+
+void Logger::write(ostream& (*pf) (ostream&), string mod)
+{
+	if(*mode=="FILE")
+	{
+		_theLogmutex->lock();
+		*out << pf;
+		_theLogmutex->unlock();
+	}
+	else
+	{
+		_theLogmutex->lock();
+		cout << pf;
+		_theLogmutex->unlock();
 	}
 }
 
 void Logger::info(string msg)
 {
-	m_pInstance->write(msg,"info");
+	if(*level!=LEVEL_DEBUG)
+	{
+		write(msg,"info");
+	}
 }
 
 void Logger::debug(string msg)
 {
-	m_pInstance->write(msg,"debug");
+	if(*level==LEVEL_DEBUG)
+	{
+		write(msg,"debug");
+	}
 }
 
 void Logger::error(string msg)
 {
-	m_pInstance->write(msg,"error");
+	write(msg,"error");
+}
+
+template <typename T>
+Logger& operator<< (Logger& logger, T msg)
+{
+	logger.write(msg,"info");
+	return logger;
+}
+Logger& operator<< (Logger& logger, ostream& (*pf) (ostream&))
+{
+	logger.write(pf,"info");
+	return logger;
 }
