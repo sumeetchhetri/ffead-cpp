65f8eae8c94e276aa27719870cf3b89dbbab5e60
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-07-20 12:54:51 +0000

    add -pthreads for solaris compiles, corrected bsd rundyn_template.sh

--- resources/rundyn_template.sh
+++ resources/rundyn_template.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $FEAD_CPP_PATH/rtdcf
 rm -f $FEAD_CPP_PATH/*.cntrl
-make all
+gmake all
 cp -Rf $FEAD_CPP_PATH/public/* $FEAD_CPP_PATH/web/default/public/
 ${Dynamic_Public_Folder_Copy}
