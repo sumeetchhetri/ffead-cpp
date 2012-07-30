c798142d810d7d2fa32cfed188e2d6a63108177d
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-07-30 15:17:56 +0000

    changes for adding Release version to web apps

---
+++ web/flexApp/src/Release/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../FlexAppController.cpp
+
+OBJS += \
+./FlexAppController.o
+
+CPP_DEPS += \
+./FlexAppController.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"../../../../include" -I"../../include" -O0 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
