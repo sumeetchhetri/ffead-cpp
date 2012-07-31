f1cb996d08409477033a72c0378eb702845f76a1
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-07-31 15:23:20 +0000

    changes for correction to make files

--- Release/makeAll.sh
+++ Release/makeAll.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-cd ../web/default/src/Release
-rm -f *.d *.o libdefault.so
-make all
-cp -f libdefault.so ../../../../lib/
-cd ../../../flexApp/src/Release
-rm -f *.d *.o libflexApp.so
-make all
-cp -f libflexApp.so ../../../../lib/
-cd ../../../oauthApp/src/Release
-rm -f *.d *.o liboauthApp.so
-make all
-cp -f liboauthApp.so ../../../../lib/
-cd ../../../../Release
 rm -f src/*.d src/*.o CHS libweblib.so
 make clean
 make all
+cd ../web/default/src/Release
+rm -f *.d *.o libdefault.so
+make all
+cp -f libdefault.so ../../../../Release/ffead-server/lib
+cd ../../../flexApp/src/Release
+rm -f *.d *.o libflexApp.so
+make all
+cp -f libflexApp.so ../../../../Release/ffead-server/lib
+cd ../../../oauthApp/src/Release
+rm -f *.d *.o liboauthApp.so
+make all
+cp -f liboauthApp.so ../../../../Release/ffead-server/lib
