9d2f4dd2847809dc37672a2b809f051306e8547e
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2012-08-03 07:35:51 +0000

    changes for installation documents and removed the fork based script shell execution

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -22,5 +22,6 @@
 
 #include "Constants.h"
 
-string Constants::INTER_LIB_FILE = "libinter.so";
-bool Constants::SYS_FORK_AVAIL = true;
+string Constants::INTER_LIB_FILE = "libinter.a";
+bool Constants::SYS_FORK_AVAIL = false;
+bool Constants::SCRIPT_EXEC_SHOW_ERRS = false;
