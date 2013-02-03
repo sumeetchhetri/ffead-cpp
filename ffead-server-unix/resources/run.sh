e82fd9d1b3b6b2465113d50bcda428d8184469ce
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2013-02-03 17:54:26 +0000

    X

--- ffead-server-unix/resources/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd $FEAD_CPP_PATH/rtdcf
-rm -f  $FEAD_CPP_PATH/lib/libinter.so
-make clean
-make all
-cp -Rf $FEAD_CPP_PATH/public/* $FEAD_CPP_PATH/web/default/public/
--- ffead-server-winx-cygwin/resources/rundyn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export FEAD_CPP_PATH=`echo $(dirname $(readlink -f $0))`
-export PATH=$FEAD_CPP_PATH/../lib:$PATH
-cd $FEAD_CPP_PATH/../rtdcf
-rm -f $FEAD_CPP_PATH/../*.cntrl
-make all
-cp -Rf $FEAD_CPP_PATH/../public/* $FEAD_CPP_PATH/../web/default/public/
