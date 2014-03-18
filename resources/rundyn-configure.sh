bf1938beecea3685bddec9ba96134e7dd9a2e820
sumeet.chhetri <sumeet.chhetri@8d2543b2-a136-46b4-a34b-a8a4570f48f8>
2014-03-18 13:19:13 +0000

    Fixed the runtime configure script Move INSTALL files to source distribution Generated initial set of configure/Makefile.in and other misc files

--- resources/rundyn-configure.sh
+++ resources/rundyn-configure.sh
@@ -2,7 +2,7 @@
 cd $FEAD_CPP_PATH/rtdcf/autotools
 rm -f $FEAD_CPP_PATH/lib/*inter.*
 rm -f Makefile
-if [ "$#" -eq 1 ] && [ "$1" -eq "reconf" ]; then
+if [ "$#" -eq 1 ] && [ "$1" = "reconf" ]; then
 	echo "Reconfiguer requested..."
 	rm -f configure
 fi
