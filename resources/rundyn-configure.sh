31ae12eaa22509ead3da641ce534951ec580940a
Sumeet <sumeet_rbc@yahoo.com>
2014-03-20 20:47:39 +0530

    Fixed the runtime configure script Move INSTALL files to source distribution Generated initial set of configure/Makefile.in and other misc files Removed generated build files for runtime libraries Updated server startup scripts Clear runtime build files during reconf

--- resources/rundyn-configure.sh
+++ resources/rundyn-configure.sh
@@ -2,9 +2,9 @@
 cd $FEAD_CPP_PATH/rtdcf/autotools
 rm -f $FEAD_CPP_PATH/lib/*inter.*
 rm -f Makefile
-if [ "$#" -eq 1 ] && [ "$1" -eq "reconf" ]; then
+if [ "$#" -eq 1 ] && [ "$1" = "reconf" ]; then
 	echo "Reconfiguer requested..."
-	rm -f configure
+	rm -f configure config.* aclocal* depcomp install-sh ltmain.sh libtool Makefile Makefile.in missing
 fi
 if [ -f configure ]; then
 	echo 'Configure file already exists'
--- script/server.sh
+++ script/server.sh
@@ -9,6 +9,13 @@ echo $PATH
 rm -f $FEAD_CPP_PATH/rtdcf/*.d $FEAD_CPP_PATH/rtdcf/*.o 
 rm -f $FEAD_CPP_PATH/*.cntrl
 rm -f $FEAD_CPP_PATH/tmp/*.sess
+if [ ! -d tmp ]; then
+mkdir tmp
+fi
+chmod 700 $FEAD_CPP_PATH/CHS
 chmod 700 $FEAD_CPP_PATH/resources/*.sh
+chmod 700 $FEAD_CPP_PATH/tests/*
+chmod 700 $FEAD_CPP_PATH/rtdcf/*
+chmod 700 $FEAD_CPP_PATH/rtdcf/autotools/*
 #/usr/sbin/setenforce 0
 ./CHS $FEAD_CPP_PATH > ffead.log 2>&1
