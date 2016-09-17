48c90e6205284353a7b66751b8364b7bd9944eb3
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2016-09-18 00:45:55 +0530

    Fixed static file issue in nginx module Module almost complete

--- modules/nginx_mod_ffeadcpp/ngx_http_ffeadcpp_module.cpp
+++ modules/nginx_mod_ffeadcpp/ngx_http_ffeadcpp_module.cpp
@@ -332,6 +332,9 @@ static ngx_int_t ngx_http_ffeadcpp_module_handler_post_read(ngx_http_request_t *
 		/* set the status line */
 		r->headers_out.status = CastUtil::lexical_cast<int>(respo->getStatusCode());
 
+		delete respo;
+		delete req;
+
 		/* send the headers of your response */
 		rc = ngx_http_send_header(r);
 
@@ -342,7 +345,204 @@ static ngx_int_t ngx_http_ffeadcpp_module_handler_post_read(ngx_http_request_t *
 	    /* send the buffer chain of your response */
 	    return ngx_http_output_filter(r, &out);
 	} else {
-		return NGX_DONE;
+		u_char                    *last, *location;
+		size_t                     len;
+		ngx_str_t                  path;
+		ngx_uint_t                 level;
+		ngx_log_t                 *log;
+		ngx_open_file_info_t       of;
+		ngx_http_core_loc_conf_t  *clcf;
+
+		clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
+		log = r->connection->log;
+
+		ngx_memzero(&of, sizeof(ngx_open_file_info_t));
+		of.read_ahead = clcf->read_ahead;
+		of.directio = clcf->directio;
+		of.valid = clcf->open_file_cache_valid;
+		of.min_uses = clcf->open_file_cache_min_uses;
+		of.errors = clcf->open_file_cache_errors;
+		of.events = clcf->open_file_cache_events;
+
+		path.data = req->getUrl().c_str();
+		path.len = ngx_strlen(path.data);
+
+		delete respo;
+		delete req;
+
+		if (ngx_http_set_disable_symlinks(r, clcf, &path, &of) != NGX_OK) {
+			return NGX_HTTP_INTERNAL_SERVER_ERROR;
+		}
+
+		if (ngx_open_cached_file(clcf->open_file_cache, &path, &of, r->pool)
+		        != NGX_OK)
+		{
+			switch (of.err) {
+
+			case 0:
+				return NGX_HTTP_INTERNAL_SERVER_ERROR;
+
+			case NGX_ENOENT:
+			case NGX_ENOTDIR:
+			case NGX_ENAMETOOLONG:
+
+				level = NGX_LOG_ERR;
+				rc = NGX_HTTP_NOT_FOUND;
+				break;
+
+			case NGX_EACCES:
+	#if (NGX_HAVE_OPENAT)
+			case NGX_EMLINK:
+			case NGX_ELOOP:
+	#endif
+
+				level = NGX_LOG_ERR;
+				rc = NGX_HTTP_FORBIDDEN;
+				break;
+
+			default:
+
+				level = NGX_LOG_CRIT;
+				rc = NGX_HTTP_INTERNAL_SERVER_ERROR;
+				break;
+			}
+
+			if (rc != NGX_HTTP_NOT_FOUND || clcf->log_not_found) {
+				ngx_log_error(level, log, of.err,
+							  "%s \"%s\" failed", of.failed, path.data);
+			}
+
+			return rc;
+		}
+
+		r->root_tested = !r->error_page;
+
+		ngx_log_debug1(NGX_LOG_DEBUG_HTTP, log, 0, "http static fd: %d", of.fd);
+
+		if (of.is_dir) {
+
+			ngx_log_debug0(NGX_LOG_DEBUG_HTTP, log, 0, "http dir");
+
+			ngx_http_clear_location(r);
+
+			r->headers_out.location = ngx_palloc(r->pool, sizeof(ngx_table_elt_t));
+			if (r->headers_out.location == NULL) {
+				return NGX_HTTP_INTERNAL_SERVER_ERROR;
+			}
+
+			len = r->uri.len + 1;
+
+			if (!clcf->alias && clcf->root_lengths == NULL && r->args.len == 0) {
+				location = path.data + clcf->root.len;
+
+				*last = '/';
+
+			} else {
+				if (r->args.len) {
+					len += r->args.len + 1;
+				}
+
+				location = ngx_pnalloc(r->pool, len);
+				if (location == NULL) {
+					return NGX_HTTP_INTERNAL_SERVER_ERROR;
+				}
+
+				last = ngx_copy(location, r->uri.data, r->uri.len);
+
+				*last = '/';
+
+				if (r->args.len) {
+					*++last = '?';
+					ngx_memcpy(++last, r->args.data, r->args.len);
+				}
+			}
+
+			/*
+			 * we do not need to set the r->headers_out.location->hash and
+			 * r->headers_out.location->key fields
+			 */
+
+			r->headers_out.location->value.len = len;
+			r->headers_out.location->value.data = location;
+
+			return NGX_HTTP_MOVED_PERMANENTLY;
+		}
+
+	#if !(NGX_WIN32) /* the not regular files are probably Unix specific */
+
+		if (!of.is_file) {
+			ngx_log_error(NGX_LOG_CRIT, log, 0,
+						  "\"%s\" is not a regular file", path.data);
+
+			return NGX_HTTP_NOT_FOUND;
+		}
+
+	#endif
+
+		if (r->method == NGX_HTTP_POST) {
+			return NGX_HTTP_NOT_ALLOWED;
+		}
+
+		rc = ngx_http_discard_request_body(r);
+
+		if (rc != NGX_OK) {
+			return rc;
+		}
+
+		log->action = "sending response to client";
+
+		r->headers_out.status = NGX_HTTP_OK;
+		r->headers_out.content_length_n = of.size;
+		r->headers_out.last_modified_time = of.mtime;
+
+		if (ngx_http_set_etag(r) != NGX_OK) {
+			return NGX_HTTP_INTERNAL_SERVER_ERROR;
+		}
+
+		if (ngx_http_set_content_type(r) != NGX_OK) {
+			return NGX_HTTP_INTERNAL_SERVER_ERROR;
+		}
+
+		if (r != r->main && of.size == 0) {
+			return ngx_http_send_header(r);
+		}
+
+		r->allow_ranges = 1;
+
+		/* we need to allocate all before the header would be sent */
+
+		b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
+		if (b == NULL) {
+			return NGX_HTTP_INTERNAL_SERVER_ERROR;
+		}
+
+		b->file = ngx_pcalloc(r->pool, sizeof(ngx_file_t));
+		if (b->file == NULL) {
+			return NGX_HTTP_INTERNAL_SERVER_ERROR;
+		}
+
+		rc = ngx_http_send_header(r);
+
+		if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+			return rc;
+		}
+
+		b->file_pos = 0;
+		b->file_last = of.size;
+
+		b->in_file = b->file_last ? 1: 0;
+		b->last_buf = (r == r->main) ? 1: 0;
+		b->last_in_chain = 1;
+
+		b->file->fd = of.fd;
+		b->file->name = path;
+		b->file->log = log;
+		b->file->directio = of.is_directio;
+
+		out.buf = b;
+		out.next = NULL;
+
+		return ngx_http_output_filter(r, &out);
 	}
 }
 
