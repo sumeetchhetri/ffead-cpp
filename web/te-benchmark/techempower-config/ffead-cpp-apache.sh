22614b8523004abb1daee488378e6b90a1063c4b
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2016-09-25 23:30:33 +0530

    Fixed te benchmark scripts

--- web/te-benchmark/techempower-config/ffead-cpp-apache.sh
+++ web/te-benchmark/techempower-config/ffead-cpp-apache.sh
@@ -7,6 +7,7 @@ fw_get -o unixODBC-2.3.4.tar.gz ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3
 fw_untar unixODBC-2.3.4.tar.gz
 cd unixODBC-2.3.4
 ./configure --enable-stats=no --enable-gui=no --enable-drivers=no --enable-iconv --with-iconv-char-enc=UTF8 --with-iconv-ucode-enc=UTF16LE --libdir=/usr/lib/x86_64-linux-gnu --prefix=/usr --sysconfdir=/etc
+sudo make install
 
 sudo apt-get install build-essential
 sudo apt-get install -y uuid-dev libmyodbc odbc-postgresql
@@ -29,8 +30,8 @@ sudo cp ${TROOT}/ffead-cpp-2.0/resources/sample-odbc.ini /etc/odbc.ini
 
 sudo sed -i 's|localhost|'${DBHOST}'|g' /etc/odbc.ini
 
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.4.0/mongo-c-driver-1.4.0.tar.gz
-tar -xzf mongo-c-driver-1.4.0.tar.gz
+fw_get -o mongo-c-driver-1.4.0.tar.gz https://github.com/mongodb/mongo-c-driver/releases/download/1.4.0/mongo-c-driver-1.4.0.tar.gz
+fw_untar mongo-c-driver-1.4.0.tar.gz
 cd mongo-c-driver-1.4.0/
 ./configure --prefix=${IROOT} --libdir=${IROOT} --disable-automatic-init-and-cleanup
 make && sudo make install
