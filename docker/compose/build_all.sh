f9e8244292176fc0fe7eeb0d8df7e9d945a7586f
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2019-03-20 15:04:14 +0530

    Added docker files for autoconf Added support for docker compose

---
+++ docker/compose/build_all.sh
@@ -0,0 +1,16 @@
+cd mongodb
+docker build -t mongo-for-ffeadcpp:latest -f mongodb.dockerfile .
+cd ../mysql
+docker build -t mysql-for-ffeadcpp:latest -f mysql.dockerfile .
+cd ../postgresql
+docker build -t postgresql-for-ffeadcpp:latest -f postgresql.dockerfile .
+cd ../ffead-cpp
+docker build -t ffead-cpp-mongo:latest -f ffead-cpp.dockerfile .
+docker build -t ffead-cpp-mysql:latest -f ffead-cpp-mysql.dockerfile .
+docker build -t ffead-cpp-postgresql:latest -f ffead-cpp-postgresql.dockerfile .
+docker build -t ffead-cpp-apache-mongo:latest -f ffead-cpp-apache.dockerfile .
+docker build -t ffead-cpp-apache-mysql:latest -f ffead-cpp-apache-mysql.dockerfile .
+docker build -t ffead-cpp-apache-postgresql:latest -f ffead-cpp-apache-postgresql.dockerfile .
+docker build -t ffead-cpp-nginx-mongo:latest -f ffead-cpp-nginx.dockerfile .
+docker build -t ffead-cpp-nginx-mysql:latest -f ffead-cpp-nginx-mysql.dockerfile .
+docker build -t ffead-cpp-nginx-postgresql:latest -f ffead-cpp-nginx-postgresql.dockerfile .
--- resources/techempower-config/old/setup-mongo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fw_depends ffead-cpp-emb-mongo
-
-chmod 755 $IROOT/ffead-cpp-2.0/*.sh
-rm -f $FFEAD_CPP_PATH/*.cntrl
-rm -f $FFEAD_CPP_PATH/tmp/*.sess
-cd $IROOT/ffead-cpp-2.0
-./server.sh
