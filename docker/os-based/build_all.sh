7cd8e4e55869453ccac1887f2cf1d2e66379e3df
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-04-22 22:00:12 +0530

    Updated docker scripts Fixed minor issues in autoconf builds

---
+++ docker/os-based/build_all.sh
@@ -0,0 +1,5 @@
+docker build -f DockerFile-ArchLinux-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-archlinux .
+docker build -f DockerFile-UbuntuBionic-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-ubuntubionic .
+docker build -f DockerFile-OpenSuseTumbleweed-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-opensusetw .
+docker build -f DockerFile-Centos7-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-centos7 .
+docker build -f DockerFile-Gentoo-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-gentoo .
---
+++ docker/webrtc-peerjs/build_all.sh
@@ -0,0 +1,1 @@
+docker build -f DockerFile-UbuntuBionic-x64-ffead-cpp-3.0 --tag sumeetchhetri/ffead-cpp-3.0:ffead-cpp-3.0-ubuntubionic-webrtc .
