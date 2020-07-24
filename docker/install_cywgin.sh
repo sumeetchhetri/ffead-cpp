270cd19cc24844c6b957288a58349493b6730f71
unknown <admin@LPT0206.ohumindia.com>
2020-07-24 14:12:41 +0100

    Added build for windows-cygwin [skip_build]

---
+++ docker/install_cywgin.sh
@@ -0,0 +1,37 @@
+cd /tmp
+wget -q https://github.com/efficient/libcuckoo/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd /tmp/libcuckoo-master
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/ .
+make install
+cd /tmp
+rm -rf libcuckoo-master
+
+wget -q http://www.unixodbc.org/unixODBC-2.3.7.tar.gz
+tar zxf unixODBC-2.3.7.tar.gz
+cd unixODBC-2.3.7
+./configure
+make
+make install
+cd /tmp
+rm -rf unixODBC-2.3.7
+
+wget -q https://github.com/redis/hiredis/archive/v0.13.3.tar.gz
+tar zxf v0.13.3.tar.gz
+rm -f v0.13.3.tar.gz
+cp net.c hiredis-0.13.3/
+cd hiredis-0.13.3/ && make && PREFIX=/usr make install
+cd /tmp
+rm -rf hiredis-0.13.3
+
+wget -q https://github.com/sumeetchhetri/ffead-cpp/archive/master.zip
+unzip master.zip
+mv ffead-cpp-master ffead-cpp-src
+rm -f master.zip
+cd /tmp/ffead-cpp-src
+cmake -DSRV_EMB=on -DMOD_REDIS=on .
+make install -j4
+mv /tmp/ffead-cpp-src/ffead-cpp-4.0-bin /tmp/
+cd /tmp
+rm -rf /tmp/ffead-cpp-src
