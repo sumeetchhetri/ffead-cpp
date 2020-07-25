1cc5a8ebf087be9ab083d778de2c103dffc134b7
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-07-25 16:23:23 +0530

    Added build for windows-mingw [skip_build]

--- docker/install_mingw.sh
+++ docker/install_mingw.sh
@@ -22,19 +22,19 @@ make install
 cd /tmp
 rm -rf ossp-uuid-master
 
-wget -q https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz
-tar xf mongo-c-driver-1.4.2.tar.gz
-rm -f mongo-c-driver-1.4.2.tar.gz
-cd mongo-c-driver-1.4.2/
-cp /tmp/ffead-cpp-src/docker/files/bson-iter.h src/libbson/src/bson/
-cp /tmp/ffead-cpp-src/docker/files/bson-compat.h src/libbson/src/bson/
-CFLAGS="-D__USE_MINGW_ANSI_STDIO=1" CFLAGS="-I/mingw64/x86_64-w64-mingw32/include" LDFLAGS="-lcrypt32" ./configure --disable-automatic-init-and-cleanup --disable-tests --prefix=/mingw64/
-make && make install
-rm -f /mingw64/include/libbson-1.0/bson-compat.h
-cp /tmp/ffead-cpp-src/docker/files/bson-compat_after.h /mingw64/include/libbson-1.0/bson-compat.h
-cp /tmp/ffead-cpp-src/docker/files/bson-prelude.h /mingw64/include/libbson-1.0/
-cd /tmp
-rm -rf mongo-c-driver-1.4.2
+#wget -q https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz
+#tar xf mongo-c-driver-1.4.2.tar.gz
+#rm -f mongo-c-driver-1.4.2.tar.gz
+#cd mongo-c-driver-1.4.2/
+#cp /tmp/ffead-cpp-src/docker/files/bson-iter.h src/libbson/src/bson/
+#cp /tmp/ffead-cpp-src/docker/files/bson-compat.h src/libbson/src/bson/
+#CFLAGS="-D__USE_MINGW_ANSI_STDIO=1" CFLAGS="-I/mingw64/x86_64-w64-mingw32/include" LDFLAGS="-lcrypt32" ./configure --disable-automatic-init-and-cleanup --disable-tests --prefix=/mingw64/
+#make && make install
+#rm -f /mingw64/include/libbson-1.0/bson-compat.h
+#cp /tmp/ffead-cpp-src/docker/files/bson-compat_after.h /mingw64/include/libbson-1.0/bson-compat.h
+#cp /tmp/ffead-cpp-src/docker/files/bson-prelude.h /mingw64/include/libbson-1.0/
+#cd /tmp
+#rm -rf mongo-c-driver-1.4.2
 
 wget -q https://github.com/efficient/libcuckoo/archive/master.zip
 unzip -qq master.zip
@@ -63,7 +63,7 @@ cd /tmp
 rm -rf hiredis-8e0264cfd6889b73c241b60736fe96ba1322ee6e
 
 cd /tmp/ffead-cpp-src
-cmake -G "MinGW Makefiles" -DSRV_EMB=on -DMOD_REDIS=on -DMOD_SDORM_MONGO=ON -DCMAKE_INC_PATH=/mingw64/ .
+cmake -G "MinGW Makefiles" -DSRV_EMB=on -DMOD_REDIS=on -DCMAKE_INC_PATH=/mingw64/ .
 mingw32-make install -j4
 mv /tmp/ffead-cpp-src/ffead-cpp-4.0-bin /tmp/
 cd /tmp
