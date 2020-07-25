6d738b63e426f725a6922e392d19f7d0493dc423
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-07-25 06:51:40 +0100

    Added build for windows-mingw [skip_build]

---
+++ docker/install_mingw.sh
@@ -0,0 +1,62 @@
+cd /tmp
+wget -q https://github.com/efficient/libcuckoo/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd /tmp/libcuckoo-master
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=/mingw64/ .
+mingw32-make install
+cd /tmp
+rm -rf libcuckoo-master
+
+wget -q http://www.unixodbc.org/unixODBC-2.3.7.tar.gz
+tar zxf unixODBC-2.3.7.tar.gz
+cd unixODBC-2.3.7
+./configure
+mingw32-make
+mingw32-make install
+cd /tmp
+rm -rf unixODBC-2.3.7
+
+wget -q https://github.com/sean-/ossp-uuid/archive/master.zip
+unzip master.zip
+rm -f master.zip
+cd ossp-uuid-master
+./configure --prefix=/mingw64/ --without-pgsql --without-perl --without-php --disable-static --enable-shared
+make
+mv uuid.exe uuid
+make install
+cd /tmp
+rm -rf ossp-uuid-master
+
+wget -q https://github.com/mongodb/mongo-c-driver/releases/download/1.4.2/mongo-c-driver-1.4.2.tar.gz
+tar xf mongo-c-driver-1.4.2.tar.gz
+rm -f mongo-c-driver-1.4.2.tar.gz
+cd mongo-c-driver-1.4.2/
+CFLAGS="-D__USE_MINGW_ANSI_STDIO=1" LDFLAGS="-lcrypt32" ./configure --disable-automatic-init-and-cleanup --disable-tests --prefix=/mingw64/
+make && make install
+rm -f /mingw64/include/libbson-1.0/bson-compat.h
+wget -q https://raw.githubusercontent.com/mongodb/mongo-c-driver/f5eb70637fdd391fe14c9a9f3ec3cfda193f3ad9/src/libbson/src/bson/bson-compat.h
+mv bson-compat.h /mingw64/include/libbson-1.0/
+wget -q https://raw.githubusercontent.com/mongodb/mongo-c-driver/f5eb70637fdd391fe14c9a9f3ec3cfda193f3ad9/src/libbson/src/bson/bson-prelude.h
+mv bson-prelude.h /mingw64/include/libbson-1.0/
+cd /tmp
+rm -rf mongo-c-driver-1.4.2
+
+wget -q https://github.com/redis/hiredis/archive/8e0264cfd6889b73c241b60736fe96ba1322ee6e.zip
+unzip 8e0264cfd6889b73c241b60736fe96ba1322ee6e.zip
+rm -f 8e0264cfd6889b73c241b60736fe96ba1322ee6e.zip
+cd hiredis-8e0264cfd6889b73c241b60736fe96ba1322ee6e
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=/mingw64/ . && mingw32-make install
+cd /tmp
+rm -rf hiredis-8e0264cfd6889b73c241b60736fe96ba1322ee6e
+
+wget -q https://github.com/sumeetchhetri/ffead-cpp/archive/master.zip
+unzip master.zip
+mv ffead-cpp-master ffead-cpp-src
+rm -f master.zip
+cd /tmp/ffead-cpp-src
+cmake -G "MinGW Makefiles" -DSRV_EMB=on -DMOD_REDIS=on -DMOD_SDORM_MONGO=ON -DCMAKE_INC_PATH=/mingw64/ .
+mingw32-make install -j4
+mv /tmp/ffead-cpp-src/ffead-cpp-4.0-bin /tmp/
+cd /tmp
+rm -rf /tmp/ffead-cpp-src
--- mingw-64-toolchain.cmake
+++ mingw-64-toolchain.cmake
@@ -12,12 +12,12 @@ set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 # cross compilers to use for C and C++
 #set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 #set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc-posix)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
-set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+#set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
-set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX} /usr/lib/gcc/${TOOLCHAIN_PREFIX}/7.3-posix)
+set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 
 
 # modify default behavior of FIND_XXX() commands to
